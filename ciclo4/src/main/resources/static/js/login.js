5a620d4f597640b6fd2ac1203e4a61e5b3ff8cfa
Brayan Garcia <brayan.garcia.mt@correo.usa.edu.co>
2021-11-21 15:48:34 -0500

    se ajusta renombra funciones.js --> login.js, se ajustan funciones

--- ciclo4/src/main/resources/static/js/login.js
+++ ciclo4/src/main/resources/static/js/login.js
@@ -1,14 +1,28 @@
+
+/**
+ * Se utiliza para que añada el evento despues
+ * de que los elementos HTML esten cargados
+ * 
+ */
 window.onload = function(){
 
     document.getElementById("botonIngresar").addEventListener("click", ingreso);
+    document.getElementById("botonRegistrar").addEventListener("click", registro);
 
 }
 
+/**
+ * URL de acceso a recurso
+ */
 const BASE_URL = "/api/user";
 
+/**
+ * funcion que se ejecuta al detectar el evento click en el boton
+ * de ingreso
+ */
 function ingreso(){
 
-    console.log("se pulso el boton");
+    console.log("se pulso el boton de Ingreso");
 
     /**
      * Se capturan datos de input email y password
@@ -18,13 +32,37 @@ function ingreso(){
 
     console.log("email --> ", $email, "password--> ", $password);
 
-    /**
-     * se ejecuta peticion GET, AJAX.
-     */
-    validarEmailPassword($email,$password);
+    if($email && $password){
+
+        /**
+         * se ejecuta peticion GET, AJAX.
+         */
+        validarEmailPassword($email,$password);
+          
+    } else{
+
+        if($email.length < 10){
+
+            alert("Por favor ingrese el correo electronico");
+            document.getElementById("email").focus();  //selecciona input email
+
+        }
+        else if($password.length < 10){
+
+            alert("Por favor ingrese la contraseña");
+            document.getElementById("password").focus();  //selecciona input password
+
+        }
+    }
 
 }
 
+/**
+ * Funcion ajax
+ * 
+ * @param {} email 
+ * @param {*} password 
+ */
 function validarEmailPassword(email,password){
 
     /**
@@ -52,7 +90,11 @@ function validarEmailPassword(email,password){
         );
 }
 
-
+/**
+ * Funcion de prueba utilizada para confirmar al usuario
+ * si los datos de inicio existen en el sistema
+ * @param {*} json 
+ */
 function respuestaIngreso(json){
 
     if(json.name != "NO DEFINIDO"){
@@ -61,5 +103,12 @@ function respuestaIngreso(json){
     else{
         alert("La informacion de inicio de sesion es INCORRECTA");
     }
+
+}
+
+
+function registro(){
+
+    console.log("se pulso el boton de Registro");
 
 }
